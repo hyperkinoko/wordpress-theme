7283aa6d2aba99f73c043e772635c32d79c8675b
hyperkinoko <superkinoko@icloud.com>
2022-11-06 14:26:33 +0900

    gaっぽいのを削除

--- html/wp-content/themes/StrategyStation/header.php
+++ html/wp-content/themes/StrategyStation/header.php
@@ -3,37 +3,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://www.google.com/"/>
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://fonts.googleapis.com/"/>
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://s.w.org/"/>
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://i0.wp.com/"/>
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://i1.wp.com/"/>
-    <link rel="dns-prefetch" href="//web.archive.org/web/20211102041247/https://i2.wp.com/"/>
-    <link rel="alternate" type="application/rss+xml" title="Strategy Station Ltd » フィード" href="<?= home_url(); ?>/feed/"/>
-    <link rel="alternate" type="application/rss+xml" title="Strategy Station Ltd » コメントフィード" href="<?= home_url(); ?>/comments/feed/"/>
-    <link rel="alternate" type="application/rss+xml" title="Strategy Station Ltd » Home のコメントのフィード" href="<?= home_url(); ?>/%e3%83%9b%e3%83%bc%e3%83%a0/feed/"/>
-    <!-- This site uses the Google Analytics by ExactMetrics plugin v7.1.0 - Using Analytics tracking - https://www.exactmetrics.com/ -->
-    <!-- Note: ExactMetrics is not currently configured on this site. The site owner needs to authenticate with Google Analytics in the ExactMetrics settings panel. -->
-    <!-- No UA code set -->
-    <!-- / Google Analytics by ExactMetrics -->
-    <script type="text/javascript">
-        window._wpemojiSettings = {"baseUrl":"http:\/\/web.archive.org\/web\/20211102041247\/https:\/\/s.w.org\/images\/core\/emoji\/13.1.0\/72x72\/","ext":".png","svgUrl":"http:\/\/web.archive.org\/web\/20211102041247\/https:\/\/s.w.org\/images\/core\/emoji\/13.1.0\/svg\/","svgExt":".svg","source":{"concatemoji":"http:\/\/web.archive.org\/web\/20211102041247\/https:\/\/strategy-station.biz\/wp-includes\/js\/wp-emoji-release.min.js?ver=5.8.1"}};
-        !function(e,a,t){var n,r,o,i=a.createElement("canvas"),p=i.getContext&&i.getContext("2d");function s(e,t){var a=String.fromCharCode;p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,e),0,0);e=i.toDataURL();return p.clearRect(0,0,i.width,i.height),p.fillText(a.apply(this,t),0,0),e===i.toDataURL()}function c(e){var t=a.createElement("script");t.src=e,t.defer=t.type="text/javascript",a.getElementsByTagName("head")[0].appendChild(t)}for(o=Array("flag","emoji"),t.supports={everything:!0,everythingExceptFlag:!0},r=0;r<o.length;r++)t.supports[o[r]]=function(e){if(!p||!p.fillText)return!1;switch(p.textBaseline="top",p.font="600 32px Arial",e){case"flag":return s([127987,65039,8205,9895,65039],[127987,65039,8203,9895,65039])?!1:!s([55356,56826,55356,56819],[55356,56826,8203,55356,56819])&&!s([55356,57332,56128,56423,56128,56418,56128,56421,56128,56430,56128,56423,56128,56447],[55356,57332,8203,56128,56423,8203,56128,56418,8203,56128,56421,8203,56128,56430,8203,56128,56423,8203,56128,56447]);case"emoji":return!s([10084,65039,8205,55357,56613],[10084,65039,8203,55357,56613])}return!1}(o[r]),t.supports.everything=t.supports.everything&&t.supports[o[r]],"flag"!==o[r]&&(t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&t.supports[o[r]]);t.supports.everythingExceptFlag=t.supports.everythingExceptFlag&&!t.supports.flag,t.DOMReady=!1,t.readyCallback=function(){t.DOMReady=!0},t.supports.everything||(n=function(){t.readyCallback()},a.addEventListener?(a.addEventListener("DOMContentLoaded",n,!1),e.addEventListener("load",n,!1)):(e.attachEvent("onload",n),a.attachEvent("onreadystatechange",function(){"complete"===a.readyState&&t.readyCallback()})),(n=t.source||{}).concatemoji?c(n.concatemoji):n.wpemoji&&n.twemoji&&(c(n.twemoji),c(n.wpemoji)))}(window,document,window._wpemojiSettings);
-    </script>
-    <style type="text/css">
-        img.wp-smiley,
-        img.emoji {
-            display: inline !important;
-            border: none !important;
-            box-shadow: none !important;
-            height: 1em !important;
-            width: 1em !important;
-            margin: 0 .07em !important;
-            vertical-align: -0.1em !important;
-            background: none !important;
-            padding: 0 !important;
-        }
-    </style>
     <link rel="stylesheet" id="wp-block-library-css" href="<?= home_url(); ?>/wp-includes/css/dist/block-library/style.min.css?ver=5.8.1" type="text/css" media="all"/>
     <style id="wp-block-library-inline-css" type="text/css">
         .has-text-align-justify{text-align:justify;}
@@ -142,8 +111,10 @@
         .stripe td {
             padding:5px;
             border:1px solid #eee;
-        }</style><style type="text/css" class="options-output">header #logo a.brand,.logofont{font-family:Lato;line-height:40px;font-weight:400;font-style:normal;font-size:32px;}.kad_tagline{font-family:Lato;line-height:20px;font-weight:400;font-style:normal;color:#444444;font-size:14px;}.product_item .product_details h5{font-family:Lato;line-height:20px;font-weight:700;font-style:normal;font-size:16px;}h1{font-family:Lato;line-height:40px;font-weight:400;font-style:normal;font-size:38px;}h2{font-family:Lora;line-height:35px;font-weight:normal;font-style:normal;font-size:30px;}h3{font-family:Lora;line-height:25px;font-weight:400;font-style:normal;color:#373764;font-size:20px;}h4{font-family:Lora;line-height:40px;font-weight:400;font-style:normal;font-size:24px;}h5{font-family:Lato;line-height:24px;font-weight:700;font-style:normal;font-size:18px;}body{font-family:Verdana, Geneva, sans-serif;line-height:20px;font-weight:400;font-style:normal;font-size:14px;}#nav-main ul.sf-menu a{font-family:Lato;line-height:18px;font-weight:400;font-style:normal;font-size:16px;}#nav-second ul.sf-menu a{font-family:Lato;line-height:22px;font-weight:400;font-style:normal;font-size:18px;}.kad-nav-inner .kad-mnav, .kad-mobile-nav .kad-nav-inner li a,.nav-trigger-case{font-family:Lato;line-height:20px;font-weight:400;font-style:normal;font-size:16px;}</style><meta name="google-translate-customization" content="92c6a7e20bd9a10b-4541acebec75b5ff-g853538fdc7fa7c59-18"></meta>
-    <meta name="google-site-verification" content="hXvIO8K6jH-X-074WTTbzNDyQziXt5e8HylCL1kw864"/>
+        }</style>
+    <style type="text/css" class="options-output">
+        header #logo a.brand,.logofont{font-family:Lato;line-height:40px;font-weight:400;font-style:normal;font-size:32px;}.kad_tagline{font-family:Lato;line-height:20px;font-weight:400;font-style:normal;color:#444444;font-size:14px;}.product_item .product_details h5{font-family:Lato;line-height:20px;font-weight:700;font-style:normal;font-size:16px;}h1{font-family:Lato;line-height:40px;font-weight:400;font-style:normal;font-size:38px;}h2{font-family:Lora;line-height:35px;font-weight:normal;font-style:normal;font-size:30px;}h3{font-family:Lora;line-height:25px;font-weight:400;font-style:normal;color:#373764;font-size:20px;}h4{font-family:Lora;line-height:40px;font-weight:400;font-style:normal;font-size:24px;}h5{font-family:Lato;line-height:24px;font-weight:700;font-style:normal;font-size:18px;}body{font-family:Verdana, Geneva, sans-serif;line-height:20px;font-weight:400;font-style:normal;font-size:14px;}#nav-main ul.sf-menu a{font-family:Lato;line-height:18px;font-weight:400;font-style:normal;font-size:16px;}#nav-second ul.sf-menu a{font-family:Lato;line-height:22px;font-weight:400;font-style:normal;font-size:18px;}.kad-nav-inner .kad-mnav, .kad-mobile-nav .kad-nav-inner li a,.nav-trigger-case{font-family:Lato;line-height:20px;font-weight:400;font-style:normal;font-size:16px;}
+    </style>
 <?php wp_head(); ?>
 </head>
 <body data-rsssl="1" class="home page-template page-template-page-sidebar page-template-page-sidebar-php page page-id-29 wp-embed-responsive wide">
